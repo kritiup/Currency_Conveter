f1210f0c3f4446864c97198fedc28dad199303f9
Kriti Upadhyay <121598632+kritiup@users.noreply.github.com>
2024-10-26 15:46:09 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const BASE URL ="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/btc.json";
+
+const dropdown = document.querySelectorAll(".dropdown select");
+
+let (code in countryList){
+    console.log(code, countryList[Code])
+}
